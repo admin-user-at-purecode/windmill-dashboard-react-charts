7cc87fb3550de1dc59338e28d71cf2882b68e997
mahendhar-varma <7093741662mahiv@gmail.com>
2024-03-14 16:21:51 +0530

    cd_1

--- src/pages/charts/Charts.js
+++ src/pages/charts/Charts.js
@@ -1,14 +1,12 @@
 import React from "react";
 
 import ChartCard, { PageTitle } from "../../components/Chart/ChartDetails.js";
-import { Doughnut, Line, Bar } from "react-chartjs-2";
+import { Line, Bar } from "react-chartjs-2";
 import ChartLegend from "../../components/Chart/ChartDetails.js";
 
 import {
-  doughnutOptions,
   lineOptions,
   barOptions,
-  doughnutLegends,
   lineLegends,
   barLegends,
 } from "../../components/Chart/chartsData.js";
