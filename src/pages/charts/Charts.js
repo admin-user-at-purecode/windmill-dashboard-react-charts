c2e857f544163f4eb2cbf0527fa40a55b5469dfb
mahendhar-varma <7093741662mahiv@gmail.com>
2024-03-14 15:24:33 +0530

    ca_3

--- src/pages/charts/Charts.js
+++ src/pages/charts/Charts.js
@@ -1,8 +1,8 @@
-import React from 'react'
+import React from "react";
 
-import ChartCard, { PageTitle } from '../../components/Chart/ChartDetails.js'
-import { Doughnut, Line, Bar } from 'react-chartjs-2'
-import ChartLegend from '../../components/Chart/ChartDetails.js'
+import ChartCard, { PageTitle } from "../../components/Chart/ChartDetails.js";
+import { Doughnut, Line, Bar } from "react-chartjs-2";
+import ChartLegend from "../../components/Chart/ChartDetails.js";
 
 import {
   doughnutOptions,
@@ -11,7 +11,7 @@ import {
   doughnutLegends,
   lineLegends,
   barLegends,
-} from '../../components/Chart/chartsData.js'
+} from "../../components/Chart/chartsData.js";
 
 function Charts() {
   return (
@@ -33,9 +33,14 @@ function Charts() {
           <Bar {...barOptions} />
           <ChartLegend legends={barLegends} />
         </ChartCard>
+
+        <ChartCard title="Bars">
+          <Bar {...barOptions} />
+          <ChartLegend legends={barLegends} />
+        </ChartCard>
       </div>
     </>
-  )
+  );
 }
 
-export default Charts
+export default Charts;
