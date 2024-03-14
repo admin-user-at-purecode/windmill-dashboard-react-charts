97cc0f550a0e156f98dcde048d1dde55f0ba0dbf
mahendhar-varma <7093741662mahiv@gmail.com>
2024-03-14 14:56:33 +0530

    la_5

--- src/pages/charts/Charts.js
+++ src/pages/charts/Charts.js
@@ -1,8 +1,8 @@
-import React from 'react'
+import React from "react";
 
-import ChartCard, { PageTitle } from '../../components/Chart/ChartDetails.js'
-import { Doughnut, Line, Bar } from 'react-chartjs-2'
-import ChartLegend from '../../components/Chart/ChartDetails.js'
+import ChartCard, { PageTitle } from "../../components/Chart/ChartDetails.js";
+import { Doughnut, Line, Bar } from "react-chartjs-2";
+import ChartLegend from "../../components/Chart/ChartDetails.js";
 
 import {
   doughnutOptions,
@@ -11,7 +11,7 @@ import {
   doughnutLegends,
   lineLegends,
   barLegends,
-} from '../../components/Chart/chartsData.js'
+} from "../../components/Chart/chartsData.js";
 
 function Charts() {
   return (
@@ -19,9 +19,9 @@ function Charts() {
       <PageTitle>Charts</PageTitle>
 
       <div className="grid gap-6 mb-8 md:grid-cols-2">
-        <ChartCard title="Doughnut">
-          <Doughnut {...doughnutOptions} />
-          <ChartLegend legends={doughnutLegends} />
+        <ChartCard title="Bars">
+          <Bar {...barOptions} />
+          <ChartLegend legends={barLegends} />
         </ChartCard>
 
         <ChartCard title="Lines">
@@ -29,13 +29,13 @@ function Charts() {
           <ChartLegend legends={lineLegends} />
         </ChartCard>
 
-        <ChartCard title="Bars">
-          <Bar {...barOptions} />
-          <ChartLegend legends={barLegends} />
+        <ChartCard title="Doughnut">
+          <Doughnut {...doughnutOptions} />
+          <ChartLegend legends={doughnutLegends} />
         </ChartCard>
       </div>
     </>
-  )
+  );
 }
 
-export default Charts
+export default Charts;
