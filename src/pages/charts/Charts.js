00967d8fe6f232fbc87bbbb31b3644a1c4f6ecb1
mahendhar-varma <7093741662mahiv@gmail.com>
2024-03-15 15:44:50 +0530

    la_8

--- src/pages/charts/Charts.js
+++ src/pages/charts/Charts.js
@@ -1,8 +1,8 @@
-import React from 'react'
-
-import ChartCard, { PageTitle } from '../../components/Chart/ChartDetails.js'
-import { Doughnut, Line, Bar } from 'react-chartjs-2'
-import ChartLegend from '../../components/Chart/ChartDetails.js'
+import React from "react";
+import { Card, CardBody } from "@windmill/react-ui";
+import ChartCard, { PageTitle } from "../../components/Chart/ChartDetails.js";
+import { Doughnut, Line, Bar } from "react-chartjs-2";
+import ChartLegend from "../../components/Chart/ChartDetails.js";
 
 import {
   doughnutOptions,
@@ -11,7 +11,7 @@ import {
   doughnutLegends,
   lineLegends,
   barLegends,
-} from '../../components/Chart/chartsData.js'
+} from "../../components/Chart/chartsData.js";
 
 function Charts() {
   return (
@@ -34,8 +34,40 @@ function Charts() {
           <ChartLegend legends={barLegends} />
         </ChartCard>
       </div>
+
+      <h2 className="mb-4 text-lg font-semibold text-gray-600 dark:text-gray-300">
+        Cards with title
+      </h2>
+
+      <div className="grid gap-6 mb-8 md:grid-cols-2">
+        <Card className="cardWithHeading">
+          <CardBody>
+            <p className="mb-4 font-semibold text-gray-600 dark:text-gray-300">
+              Revenue
+            </p>
+            <p className="text-gray-600 dark:text-gray-400">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fuga,
+              cum commodi a omnis numquam quod? Totam exercitationem quos hic
+              ipsam at qui cum numquam, sed amet ratione! Ratione, nihil
+              dolorum.
+            </p>
+          </CardBody>
+        </Card>
+
+        <Card colored className="text-white bg-purple-600 cardWithHeading">
+          <CardBody>
+            <p className="mb-4 font-semibold">Colored card</p>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fuga,
+              cum commodi a omnis numquam quod? Totam exercitationem quos hic
+              ipsam at qui cum numquam, sed amet ratione! Ratione, nihil
+              dolorum.
+            </p>
+          </CardBody>
+        </Card>
+      </div>
     </>
-  )
+  );
 }
 
-export default Charts
+export default Charts;
