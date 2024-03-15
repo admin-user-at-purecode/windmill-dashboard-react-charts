2a2bf8716c7d902778d7a8d0daf869d26a8d699c
mahendhar-varma <7093741662mahiv@gmail.com>
2024-03-15 15:16:54 +0530

    ca_7

--- src/pages/charts/Charts.js
+++ src/pages/charts/Charts.js
@@ -1,8 +1,10 @@
-import React from 'react'
+import React from "react";
+import InfoCard, { RoundIcon } from "../../components/card/InfoCard.js";
+import ChartCard, { PageTitle } from "../../components/Chart/ChartDetails.js";
+import { Doughnut, Line, Bar } from "react-chartjs-2";
+import ChartLegend from "../../components/Chart/ChartDetails.js";
 
-import ChartCard, { PageTitle } from '../../components/Chart/ChartDetails.js'
-import { Doughnut, Line, Bar } from 'react-chartjs-2'
-import ChartLegend from '../../components/Chart/ChartDetails.js'
+import { CardsIcon } from "../../assets/icons";
 
 import {
   doughnutOptions,
@@ -11,12 +13,50 @@ import {
   doughnutLegends,
   lineLegends,
   barLegends,
-} from '../../components/Chart/chartsData.js'
+} from "../../components/Chart/chartsData.js";
 
 function Charts() {
   return (
     <>
       <PageTitle>Charts</PageTitle>
+
+      <div className="grid gap-6 mb-8 md:grid-cols-2 xl:grid-cols-4">
+        <InfoCard title="Total clients" value="6389">
+          <RoundIcon
+            icon={CardsIcon}
+            iconColorClass="text-orange-500 dark:text-orange-100"
+            bgColorClass="bg-orange-100 dark:bg-orange-500"
+            className="mr-4"
+          />
+        </InfoCard>
+
+        <InfoCard title="Account balance" value="$ 46,760.89">
+          <RoundIcon
+            icon={CardsIcon}
+            iconColorClass="text-green-500 dark:text-green-100"
+            bgColorClass="bg-green-100 dark:bg-green-500"
+            className="mr-4"
+          />
+        </InfoCard>
+
+        <InfoCard title="New sales" value="376">
+          <RoundIcon
+            icon={CardsIcon}
+            iconColorClass="text-blue-500 dark:text-blue-100"
+            bgColorClass="bg-blue-100 dark:bg-blue-500"
+            className="mr-4"
+          />
+        </InfoCard>
+
+        <InfoCard title="Pending contacts" value="35">
+          <RoundIcon
+            icon={CardsIcon}
+            iconColorClass="text-teal-500 dark:text-teal-100"
+            bgColorClass="bg-teal-100 dark:bg-teal-500"
+            className="mr-4"
+          />
+        </InfoCard>
+      </div>
 
       <div className="grid gap-6 mb-8 md:grid-cols-2">
         <ChartCard title="Doughnut">
@@ -35,7 +75,7 @@ function Charts() {
         </ChartCard>
       </div>
     </>
-  )
+  );
 }
 
-export default Charts
+export default Charts;
