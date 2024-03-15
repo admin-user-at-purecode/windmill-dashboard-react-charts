d1be53f3c301e5529439e789585d60cce68792d2
mahendhar-varma <7093741662mahiv@gmail.com>
2024-03-15 13:00:07 +0530

    ca_6

--- src/pages/charts/Charts.js
+++ src/pages/charts/Charts.js
@@ -1,8 +1,8 @@
-import React from 'react'
-
-import ChartCard, { PageTitle } from '../../components/Chart/ChartDetails.js'
-import { Doughnut, Line, Bar } from 'react-chartjs-2'
-import ChartLegend from '../../components/Chart/ChartDetails.js'
+import React from "react";
+import { Card, CardBody } from "@windmill/react-ui";
+import ChartCard, { PageTitle } from "../../components/Chart/ChartDetails.js";
+import { Doughnut, Line, Bar } from "react-chartjs-2";
+import ChartLegend from "../../components/Chart/ChartDetails.js";
 
 import {
   doughnutOptions,
@@ -11,12 +11,26 @@ import {
   doughnutLegends,
   lineLegends,
   barLegends,
-} from '../../components/Chart/chartsData.js'
+} from "../../components/Chart/chartsData.js";
 
 function Charts() {
   return (
     <>
       <PageTitle>Charts</PageTitle>
+
+      <h2 className="mb-4 text-lg font-semibold text-gray-600 dark:text-gray-300">
+        Big section cards
+      </h2>
+
+      <div className="fullWidthCard">
+        <Card className="mb-8 shadow-md">
+          <CardBody>
+            <p className="text-sm text-gray-600 dark:text-gray-400">
+              Large, full width sections goes here
+            </p>
+          </CardBody>
+        </Card>
+      </div>
 
       <div className="grid gap-6 mb-8 md:grid-cols-2">
         <ChartCard title="Doughnut">
@@ -35,7 +49,7 @@ function Charts() {
         </ChartCard>
       </div>
     </>
-  )
+  );
 }
 
-export default Charts
+export default Charts;
