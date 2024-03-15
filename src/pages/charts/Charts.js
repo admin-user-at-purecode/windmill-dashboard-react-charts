106fb9742436c57b9ebd252552d0a562d6e791ae
mahendhar-varma <7093741662mahiv@gmail.com>
2024-03-15 17:09:14 +0530

    ca_9

--- src/pages/charts/Charts.js
+++ src/pages/charts/Charts.js
@@ -1,8 +1,8 @@
-import React from 'react'
-
-import ChartCard, { PageTitle } from '../../components/Chart/ChartDetails.js'
-import { Doughnut, Line, Bar } from 'react-chartjs-2'
-import ChartLegend from '../../components/Chart/ChartDetails.js'
+import React from "react";
+import { Card, CardBody } from "@windmill/react-ui";
+import ChartCard, { PageTitle } from "../../components/Chart/ChartDetails.js";
+import { Doughnut, Line, Bar } from "react-chartjs-2";
+import ChartLegend from "../../components/Chart/ChartDetails.js";
 
 import {
   doughnutOptions,
@@ -11,12 +11,39 @@ import {
   doughnutLegends,
   lineLegends,
   barLegends,
-} from '../../components/Chart/chartsData.js'
+} from "../../components/Chart/chartsData.js";
 
 function Charts() {
   return (
     <>
       <PageTitle>Charts</PageTitle>
+
+      <a className="CTA flex items-center justify-between p-4 mb-8 text-sm font-semibold text-purple-100 bg-purple-600 rounded-lg shadow-md focus:outline-none focus:shadow-outline-purple">
+        <div className="flex items-center">
+          <svg className="w-5 h-5 mr-2" fill="currentColor" viewBox="0 0 20 20">
+            <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+          </svg>
+          <span>Star this project on GitHub</span>
+        </div>
+        <span>
+          View more{" "}
+          <span dangerouslySetInnerHTML={{ __html: "&RightArrow;" }}></span>
+        </span>
+      </a>
+
+      <h2 className="mb-4 text-lg font-semibold text-gray-600 dark:text-gray-300">
+        Big section cards
+      </h2>
+
+      <div className="fullWidthCard">
+        <Card className="mb-8 shadow-md">
+          <CardBody>
+            <p className="text-sm text-gray-600 dark:text-gray-400">
+              Large, full width sections goes here
+            </p>
+          </CardBody>
+        </Card>
+      </div>
 
       <div className="grid gap-6 mb-8 md:grid-cols-2">
         <ChartCard title="Doughnut">
@@ -35,7 +62,7 @@ function Charts() {
         </ChartCard>
       </div>
     </>
-  )
+  );
 }
 
-export default Charts
+export default Charts;
